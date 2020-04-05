60cc0d2cab5946c46f430c25870f90cfa313a62e
FabioOrlinski <fabio.orlinski@gmail.com>
2020-04-05 18:13:30 -0300

    Agrego getters, setters y contructores

--- Usuario.java
+++ Usuario.java
@@ -5,4 +5,69 @@ public class Usuario {
 	private int edad;
 	private int cantHijos;
 	private int saldo;
+
+	//Getters y setters
+
+	public void setNombre(String unNombre) {
+		nombre = unNombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setCedula(String unaCedula) {
+		cedula = unaCedula;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setContraseña(String unaContraseña) {
+		contraseña = unaContraseña;
+	}
+
+	public String getContraseña() {
+		return contraseña;
+	}
+
+	public void setEdad(int unaEdad) {
+		edad = unaEdad;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setCantHijos(int unaCantHijos) {
+		cantHijos = unaCantHijos;
+	}
+
+	public int getCantHijos() {
+		return cantHijos;
+	}
+
+	public void setSaldo(int unSaldo) {
+		saldo = unSaldo;
+	}
+
+	public int getSaldo() {
+		return saldo;
+	}
+
+	//Constructores
+
+	public Usuario() {
+		this.setSaldo(0);
+	}
+
+	public Usuario(Sting unNombre, String unaCedula, String unaContraseña, int unaEdad, int unaCantHijos, int unSaldo) {
+		this.setNombre(unNombre);
+		this.setCedula(unaCedula);
+		this.setContraseña(unaContraseña);
+		this.setEdad(unaEdad);
+		this.setCantHijos(unaCantHijos);
+		this.setSaldo(unSaldo);
+	}
 }
